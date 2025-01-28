d4ce42f79d2d7b18a3f9a487451f21650fa94ced
SHANKEKR4K <medjahdiismail1998@gmail.com>
2025-01-28 08:59:12 +0100

    welcome page

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,17 @@
-import Nav from "@/components/Nav";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export default function PrivatePage() {
-  return <Nav />;
+  return (
+    <div className="flex flex-col justify-center items-center h-screen border p-2 gap-10 ">
+      <div className="text-xl">Welcome to my chatApp</div>
+      <div className="flex gap-5">
+        <Link href="/login">
+          <Button>Login</Button>
+        </Link>
+        <Link href="/register">
+          <Button>register</Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
